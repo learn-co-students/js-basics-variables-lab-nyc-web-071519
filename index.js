80f9f9ad906750ee112d5d7729d44b78b50dee38
github username <github email address>
2019-08-26 09:14:16 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const companyName = "Scuber";
 
-var mostProfitableNeighborhood = "Chelsea";
+let mostProfitableNeighborhood = "Chelsea";
 
-var companyCeo = "Susan Smith";
+let companyCeo = "Susan Smith";
